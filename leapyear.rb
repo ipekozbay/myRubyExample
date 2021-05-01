3a1669ba7d893e391aa0ef6c3de43282246aa98e
İpek Özbay <72815766+ipekozbay@users.noreply.github.com>
2021-05-01 22:22:43 +0300

    Add files via upload

---
+++ leapyear.rb
@@ -0,0 +1,18 @@
+k=gets.chomp.to_i
+t=0
+
+while t < k
+    t+=1
+
+    year=gets.chomp.to_i
+
+    if year%4==0 && 1000 <= year &&  year <= 100000
+        puts "yes"
+
+    elsif  year%4!=0 && 1000 <=year  && year <= 100000
+        puts "no"
+
+    else    
+
+    end
+end
