ef4639b877ec469223ce11da934c03aadacc0590
İpek Özbay <20060401@stu.omu.edu.tr>
2021-06-07 00:50:59 +0300

    Add files via upload
     Ortadan ikiye ayrildiginda ortaya cikan sayıların toplamının karesine eşit olan dört basamaklı sayılara Köksal Sayı dendiğini varsayınız. Buna göre klavyeden rakamları tek tek girilen 4 basamaklı bir sayının Köksal Sayı olup olmadığını bularak ekrana yazan program. 
     ornegin ABCD ikiye ayrılınca AB CD OLUSUR. AB +CD = X OLSUN
     X**2 =ABCD ISE ABCD KOKSAL SAYIDIR

---
+++ specialnum.rb
@@ -0,0 +1,31 @@
+
+
+print "dort basamakli sayinin ilk rakamini giriniz: "
+ilkrakam = gets.to_i
+
+print "dort basamakli sayinin ikinci rakamini giriniz:"
+ikincirakam = gets.to_i
+
+print "dort basamakli sayinin ucuncu rakamini giriniz: "
+ucuncurakam = gets.to_i
+
+print "dort basamakli sayinin dorduncu rakamini giriniz: "
+dorduncurakam = gets.to_i
+
+print "\n" 
+ABCD = ("#{ilkrakam}#{ikincirakam}#{ucuncurakam}#{dorduncurakam}").to_i
+
+print "girilen sayi:#{ilkrakam}#{ikincirakam}#{ucuncurakam}#{dorduncurakam}"
+print ",ikiye ayrilmis durumda #{ilkrakam}#{ikincirakam} ve #{ucuncurakam}#{dorduncurakam} ,\n"
+
+AB = "#{ilkrakam}#{ikincirakam}"
+CD = "#{ucuncurakam}#{dorduncurakam}"
+
+x = AB.to_i + CD.to_i 
+print "ikiye ayrilmis sayilarin toplami:#{x} olmaktadir."
+
+if x*x == ABCD
+    print "Sayiniz koksal sayidir" 
+else 
+    print "Sayiniz koksal sayi degildir"
+end
